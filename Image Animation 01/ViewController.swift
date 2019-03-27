7c57122c18561c9f5b1634de67aa0f1924f21f0e
dit03 <dit@imac-4.local>
2019-03-27 14:48:28 +0900

    first Commit

--- Image Animation 01/ViewController.swift
+++ Image Animation 01/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lblCount: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var count = 1
+    var dire = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblCount.text = "\(count)번째 사진"
+        imageView.image = UIImage(named: "frame1.png")
+        
+        
+        
     }
 
-
+    @IBAction func Update(_ sender: Any) {
+//        if count < 5 {
+//            count+=1
+//        } else {
+//            count = 1
+//        }
+        
+        if dire == false {
+            if count < 5 {
+                count += 1
+                if count == 5 {
+                    dire = true
+                }
+            }
+        } else {
+            if count > 1 {
+                count -= 1
+                if count == 1 {
+                    dire = false
+                }
+            }
+        }
+        
+        lblCount.text = "\(count)번째 사진"
+        imageView.image = UIImage(named: "frame\(count).png")
+    }
+    
 }
 
